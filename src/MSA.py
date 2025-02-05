6e4d98dc3c3e7ec72f58f772068624d3dd2791a3
Elijah Morgan <morgaeli@mail.gvsu.edu>
2025-02-05 16:41:55 -0500

    Restructuring code for multithreading (multiple clients)

--- src/MSA.py
+++ src/MSA.py
@@ -2,14 +2,19 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from threading import Thread
 from time import sleep
 
-# Me when multi-threading :O
+# Me when the multi-thread :O
+
 #def someWork(val):
 #    for k in range(20):
 #        print(f"In function {x}")
 #        sleep(0.4)   # 400 milliseconds
+#
+#t1 = Thread(target = someWork, args=(51,))
+#t1.start()
+#for m in range(20):
+#    print(f"In Main {m}")
+#    sleep(0.25)   # 250 milliseconds
 
-# NOTE Remember, all sent mail just goes to /dev/null
-# NUSA: NUll Submission Agent
 '''
 C: (Initiate connection)
                                    S: (Accept connection request)
@@ -50,29 +55,64 @@ ail server which will never fill up your mailbox?\r
 \n.\r\n
 '''
 
+class connectionState(Enum):
+    INIT = 0
+    HELO = 1
+    #EHLO = # I'm assuming we're not worried about extended SMTP. Maybe I add more cases later
+    MAIL = 2
+    RCPT = 3
+    DATA = 4
+    QUIT = 5
+
+
+def recieveClientdata(clientSocket) -> tuple[bytearray]:
+    clientSocket.sendall(b"220 localhost NUSA\r\n")
+    body = bytearray()
+    state = connectionState.INIT
+
+    while True:
+        match state:
+            case connectionState.INIT:
+                ...
+            case connectionState.EHLO:
+                ...
+            case connectionState.HELO:
+                ...
+            case connectionState.MAIL:
+                ...
+            case connectionState.RCPT:
+                ...
+            case connectionState.DATA:
+                ...
+            case connectionState.QUIT:
+                break
+    
+
+
+# NOTE Remember, all sent mail just goes to /dev/null
+# NUSA: NUll Submission Agent
+
 def main():
     # Create a TCP socket that listens to port 9000 on the local host
     welcomeSocket = socket(AF_INET, SOCK_STREAM) 
     welcomeSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # As per professor's suggestion
     welcomeSocket.bind(("", 9000))
     welcomeSocket.listen(4)    # Max backlog 4 connections
-    
-    # NOTE Multithreading sample.
-    #t1 = Thread(target = someWork, args=(51,))
-    #t1.start()
-    #for m in range(20):
-    #    print(f"In Main {m}")
-    #    sleep(0.25)   # 250 milliseconds
+    bytesRecieved = 0
+    data = bytearray()
 
     # Args must be a list
     print ('Server is listening on port 9000')
     connectionSocket, addr = welcomeSocket.accept()
     print ("Accept a new connection", addr)
 
-    # Read AT MOST 1024 bytes from the socket
+    connectionData = Thread(target = recieveClientdata, args=(connectionSocket,))
+    connectionData.start()
+
     # decode(): converts bytes to text
     # encode(): convert text to bytes
-    text = connectionSocket.recv(1024).decode()
+
+    text = data.decode() # Recieves data in Kilobytes
     print (f"Incoming text is {text}")
     connectionSocket.sendall("This is a sample text".encode())
     connectionSocket.close()
